81f6985be5b91694a6b1bc928e7cf15795c2fc51
OriginalChoom <86567990+OriginalChoom@users.noreply.github.com>
2023-05-25 17:16:41 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include "header.h"
 
-//4
+//4 6
 void ispisFilmova(FILM* filmovi, int); //ispis filmova
 void dodajFilm(FILM* filmovi, int); //dodavanje filma
 void brisiFilm(FILM filmovi[], int* brojacFilmova); //brisanje filma
